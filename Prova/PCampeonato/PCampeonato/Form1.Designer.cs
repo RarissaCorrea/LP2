0fc1f30eef34b6156c63d9cea4bb1103cba714b8
Rarissa Correa <111161383+RarissaCorrea@users.noreply.github.com>
2024-08-08 15:03:49 -0300

    Add files via upload

---
+++ Prova/PCampeonato/PCampeonato/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace PCampeonato
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.lbxResultado = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExecutar.Location = new System.Drawing.Point(82, 147);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(150, 150);
+            this.btnExecutar.TabIndex = 0;
+            this.btnExecutar.Text = "Executar";
+            this.btnExecutar.UseVisualStyleBackColor = true;
+            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // lbxResultado
+            // 
+            this.lbxResultado.FormattingEnabled = true;
+            this.lbxResultado.Location = new System.Drawing.Point(323, 31);
+            this.lbxResultado.Name = "lbxResultado";
+            this.lbxResultado.Size = new System.Drawing.Size(331, 394);
+            this.lbxResultado.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 462);
+            this.Controls.Add(this.lbxResultado);
+            this.Controls.Add(this.btnExecutar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExecutar;
+        private System.Windows.Forms.ListBox lbxResultado;
+    }
+}
+
---
+++ Prova/PCampeonato/PCampeonato/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace PCampeonato
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Julia Cassemiro e Rarissa Silva Correa
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+            int[,] Gols = new int[9, 3];
+            string golsFeitos, golsReceb;
+            int totalFeitos=0, totalRecebidos=0;
+
+            for(int i = 0; i < 9; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    if (j == 0)
+                    {
+                        golsFeitos = Interaction.InputBox("Digite gols feitos pelo time " + (i + 1).ToString(), "Entrada de dados");
+
+                        if (!Int32.TryParse(golsFeitos, out Gols[i, j]))
+                        {
+                            MessageBox.Show("Insira quantidade de gols válida");
+                            j--;
+                        }
+                        else if (Gols[i, j] < 0)
+                        {
+                            MessageBox.Show("Insira quantidade de gols válida");
+                            j--;
+                        }
+                        else
+                            totalFeitos += Gols[i, j];
+                    }
+
+
+                    if (j == 1)
+                    {
+                        golsReceb = Interaction.InputBox("Digite gols recebidos pelo time " + (i + 1).ToString(), "Entrada de dados");
+
+                        if (!Int32.TryParse(golsReceb, out Gols[i, j]))
+                        {
+                            MessageBox.Show("Insira quantidade de gols válida");
+                            j--;
+                        }
+                        else if (Gols[i, j] < 0)
+                        {
+                            MessageBox.Show("Insira quantidade de gols válida");
+                            j--;
+                        }
+                        else
+                            totalRecebidos += Gols[i, j];
+                    }
+
+                    if (j == 2)
+                    {
+                        Gols[i, j] = Gols[i, 0] - Gols[i, 1];
+                    }
+                }
+                lbxResultado.Items.Add("Time:"+ (i + 1).ToString() + " Gols feitos:" + Gols[i,0].ToString() + " Gols recebidos:" + Gols[i,1].ToString() + " Saldo de gols:" + Gols[i, 2].ToString());
+            }
+            lbxResultado.Items.Add("-------------------------------------------------------------------------------------");
+            lbxResultado.Items.Add("Total gols feitos:" + totalFeitos.ToString());
+            lbxResultado.Items.Add("Total gols recebidos:" + totalRecebidos.ToString());
+        }
+    }
+}
